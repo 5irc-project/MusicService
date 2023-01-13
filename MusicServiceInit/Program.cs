43c656dca134c0206c1bd1e97671ebf05420b82b
Florian MONEDIERE <florian.monediere@cpe.fr>
2023-01-13 13:45:20 +0100

    added MusicServiceInit with reference to MusicService

---
+++ MusicServiceInit/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MusicServiceInit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
