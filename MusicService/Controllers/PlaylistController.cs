b3ca391ab6d11856aa332ffb0fc85a4cb89d0ccd
Zennyth <mathis.figuet@orange.fr>
2023-01-19 00:14:08 +0100

    add mock for recommendation

--- MusicService/Controllers/PlaylistController.cs
+++ MusicService/Controllers/PlaylistController.cs
@@ -160,13 +160,13 @@ namespace MusicService.Controllers
         }
 
         // POST: api/Recommendation
-        // [Authorize]
+        [Authorize]
         [HttpPost("Recommendation/Dev")]
         public async Task<IActionResult> GeneratePlaylistDev(List<TrackDTO> listTrack) // TO REMOVE
         {
             try{
-                // var userId = GetUserIdFromClaims();
-                PlaylistDTO pCreated = await _service.GeneratePlaylistDev(listTrack, 10);
+                var userId = GetUserIdFromClaims();
+                PlaylistDTO pCreated = await _service.GeneratePlaylistDev(listTrack, userId);
                 return CreatedAtAction("GetPlaylist", new { id = pCreated.PlaylistId }, pCreated);
             }catch(BadRequestException e){
                 return BadRequest(e.Content);
