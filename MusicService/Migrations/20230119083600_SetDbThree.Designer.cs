d2811611a74b1ff0bb08dda355b6844a4d542642
sah0428 <mathieu.Eriau@softathome.com>
2023-01-19 09:36:25 +0100

    added migration

--- MusicService/Migrations/20230119083600_SetDbThree.Designer.cs
+++ MusicService/Migrations/20230119083600_SetDbThree.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicService.Models;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MusicService.Migrations
 {
     [DbContext(typeof(MusicServiceDBContext))]
-    partial class MusicServiceDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230119083600_SetDbThree")]
+    partial class SetDbThree
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MusicService/Migrations/20230119083600_SetDbThree.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicService.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDbThree : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valence",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Tempo",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Speechiness",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Popularity",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Loudness",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Liveness",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Instrumentalness",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Energy",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DurationMs",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Danceability",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Acousticness",
+                table: "Tracks",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Valence",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Tempo",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Speechiness",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Popularity",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Loudness",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Liveness",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Instrumentalness",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Energy",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "DurationMs",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Danceability",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Acousticness",
+                table: "Tracks",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+    }
+}
